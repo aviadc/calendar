5b25611e855ba00d55fdc7f8cdf144d9842912d6
aviadc <coaviadco@gmail.com>
2022-08-05 18:56:19 +0300

    weekday css

---
+++ src/components/AddTask.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="add-task-container">
+    <label for="task">Task</label>
+    <input type="text" />
+    <label for="time">Time</label>
+    <input type="text" />
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="task-container">
+    <div class="task-left"></div>
+    <div class="task-inner">
+      <h3 class="task-title"></h3>
+      <h4 class="task-time"></h4>
+    </div>
+    <div class="delete-task">
+      <button>X</button>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+</style>
